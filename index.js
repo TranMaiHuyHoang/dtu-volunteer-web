6aa99a74999930407c0d1b9034dcad44a301c279
dangtuanminh <tuanminhtg004@gmail.com>
2025-10-27 16:24:00 +0700

    set up swagger and change routes in index file

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const app = express();
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const path = require('path');
-var auth = require('./routes/auth');
-const profile = require('./routes/welcome.js');
+const mainRouter = require('./routes/index');
 // Set up Global configuration access
 const dotenv = require('dotenv');
 const flash = require('connect-flash');
@@ -14,11 +13,24 @@ var morgan = require('morgan')
 // HTTP request logger middleware for node.js
 app.use(morgan('tiny')); 
 require("./utils/passportConfig.js");
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpecs = require('./swagger'); // Import file cấu hình Swagger
+const { errorHandler } = require('./middlewares/errorHandler.js');
+
+
 
 dotenv.config();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// Middleware cho API docs
+const swaggerEndpoint = '/api-docs';
+app.use(
+  swaggerEndpoint, 
+  swaggerUi.serve, 
+  swaggerUi.setup(swaggerSpecs, { explorer: true })
+);
 
 
 // Use express - session middleware
@@ -56,17 +68,17 @@ app.use(express.json());
 // Parse form data
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', auth);
-app.use('/profile', profile );
 
 
 // Thêm middleware để ghi log mỗi khi có yêu cầu đến server
 app.use((req, res, next) => {
   console.log('Middleware đang chạy');
+  console.log('Middleware A: Đang kiểm tra URL:', req.originalUrl);
   next();
 });
 
-
+app.use('/', mainRouter);
+app.use(errorHandler); // Đảm bảo middleware xử lý lỗi được đặt sau tất cả các route khác
 
 
 let PORT = process.env.PORT || 3000;
@@ -80,5 +92,6 @@ connectToMongoDB().catch((err) => console.error(err))
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Docs có sẵn tại: http://localhost:${PORT}${swaggerEndpoint}`);
 });
 
