1801cc3f04c324d4a2e15e764330df255f466f22
dangtuanminh <tuanminhtg004@gmail.com>
2025-11-03 22:47:31 +0700

    refactor: extract cookie utilities for setting/clearing accessToken cookie

---
+++ utils/authCookie.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+/**
+ * Utility module để quản lý accessToken cookie
+ * Giúp giảm code lặp và đảm bảo nhất quán trong cách set/clear cookie
+ */
+
+/**
+ * Set accessToken vào HTTP-only cookie
+ * @param {object} res - Express response object
+ * @param {string} token - JWT access token
+ */
+const setAccessTokenCookie = (res, token) => {
+    res.cookie('accessToken', token, {
+        httpOnly: true, // Ngăn JavaScript truy cập (bảo mật)
+        secure: process.env.NODE_ENV === 'production', // Chỉ gửi qua HTTPS trong production
+        sameSite: 'lax', // CSRF protection
+        maxAge: 24 * 60 * 60 * 1000 // 24 giờ (tương ứng với JWT_EXPIRES_IN)
+    });
+};
+
+/**
+ * Clear accessToken cookie
+ * @param {object} res - Express response object
+ */
+const clearAccessTokenCookie = (res) => {
+    res.clearCookie('accessToken', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax'
+    });
+};
+
+module.exports = {
+    setAccessTokenCookie,
+    clearAccessTokenCookie
+};
