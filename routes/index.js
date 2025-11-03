a1dfa2f36e4573a350fb02caa76f738e5249ade1
dangtuanminh <tuanminhtg004@gmail.com>
2025-11-03 22:51:18 +0700

    refactor: update routes index and remove old registration routes

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,9 @@ const { verifyToken } = require('../middlewares/jwt-auth.middleware.js');
 const auth = require('./auth.routes.js');
 const profile = require('./welcome.js');
 const activityRouter = require('./activity.routes.js');
-const registrationRouter = require('./registrationRoutes.js');
+const registrationRouter = require('./registration.routes.js');
 const notificationRouter = require('./notification.routes.js');
+const projectRouter = require('./project.routes.js');
 //const regCtrl = require('../controllers/registration.controller');
 
 
@@ -17,6 +18,8 @@ router.use('/registrations', verifyToken, registrationRouter);
 router.use('/', auth);
 router.use('/profile', verifyToken, profile );
 router.use('/notifications', notificationRouter);
+router.use('/projects', projectRouter);
+
 // router.get('/me', regCtrl.listMyRegistrations);
 
 module.exports = router;
